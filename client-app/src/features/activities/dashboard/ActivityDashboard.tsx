dc2c4827131bedb80361e41cb45b8f8cad67335f
David Lape <davidlape02@gmail.com>
2022-06-14 11:37:44 -0500

    Fixed client-app not adding to repo

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import ActivityList from './ActivityList';
+import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityFilters from './ActivityFilters';
+
+
+export default observer(function ActivityDashboard() {
+
+    const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+       if(activityRegistry.size <= 1) activityStore.loadActivities();
+    }, [activityRegistry.size, loadActivities, activityStore])
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                <ActivityFilters />
+            </Grid.Column>
+        </Grid>
+    )
+})
