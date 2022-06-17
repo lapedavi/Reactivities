f1118b814b57113df6527f7d20d8c44337cd2666
David Lape <davidlape02@gmail.com>
2022-06-17 10:28:27 -0500

    Go Back Commit

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,18 +1,22 @@
-﻿using Application.Core;
+﻿using API.Extensions;
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers {
+namespace API.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseAPIController : ControllerBase {
+    public class BaseAPIController : ControllerBase
+    {
 
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
-        protected ActionResult HandleResults<T>(Result<T> result) {
+        protected ActionResult HandleResults<T>(Result<T> result)
+        {
             if (result == null)
                 return NotFound();
             if (result.isSucess && result.value != null)
@@ -23,6 +27,21 @@ namespace API.Controllers {
             return BadRequest(result.Error);
         }
 
+        protected ActionResult HandlePagedResults<T>(Result<PagedList<T>> result)
+        {
+            if (result == null)
+                return NotFound();
+            if (result.isSucess && result.value != null)
+            {
+                Response.AddPaginationHeader(result.value.CurrentPage, result.value.PageSize,
+                result.value.TotalCount, result.value.TotalPages);
+                return Ok(result.value);
+            }
+            if (result.isSucess && result.value == null)
+                return NotFound();
+
+            return BadRequest(result.Error);
+        }
+
     }
 }
- 
